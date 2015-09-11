41a250139f7fc86123ecf82d99cf93d4efcb9d6c
jburks17 <jburks17@icould.com>
2015-09-11 10:59:54 -0400

    test1
    first test program

---
+++ test1/test1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test1
+//
+//  Created by Josh Burks on 9/11/15.
+//  Copyright (c) 2015 Josh Burks. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
